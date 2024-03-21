4645e0bf5cbf741e302b3be523a2fd9acfcfd5db
Guillaume Doré <gdore@escardio.org>
2024-03-21 14:13:51 +0100

    add extension method for managing IServiceCollection init.

--- src/GoldenGui.Hive.Functions/Program.cs
+++ src/GoldenGui.Hive.Functions/Program.cs
@@ -1,4 +1,4 @@
-using GoldenGui.Hive.Core.Configuration;
+using GoldenGui.Hive.Functions;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,10 +7,7 @@ var host = new HostBuilder()
 	.ConfigureFunctionsWorkerDefaults()
 	.ConfigureServices((context, services) =>
 	{
-		services.AddOptions<BinanceOptions>()
-			.BindConfiguration(BinanceOptions.Section)
-			.ValidateDataAnnotations()
-			.ValidateOnStart();
+		services.AddExchangeOptions();
 
 		services.AddApplicationInsightsTelemetryWorkerService();
 		services.ConfigureFunctionsApplicationInsights();
---
+++ src/GoldenGui.Hive.Functions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using GoldenGui.Hive.Core.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GoldenGui.Hive.Functions;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddExchangeOptions(this IServiceCollection services)
+	{
+		services.AddOptions<BinanceOptions>()
+			.BindConfiguration(BinanceOptions.Section)
+			.ValidateDataAnnotations()
+			.ValidateOnStart();
+
+		return services;
+	}
+}
